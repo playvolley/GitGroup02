3ce3466d7c4a89bd1d9983e0f8ab234b0fcd399a
Reachthestar <nystar.aus@gmail.com>
2024-06-19 14:59:40 +0700

    login ok

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
-const express = require("express");
+const express = require('express');
 
 const app = express();
 
+app.use('/login', (req, res, next) => {
+  res.status(200).json({ msg: 'login successfully' });
+});
+
 app.listen(8888, () => {
-  console.log("server on 8888");
+  console.log('server on 8888');
 });
